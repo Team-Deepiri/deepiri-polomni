35dd044d686d456017e1a0a2986188377f68b09b
jrb00013 <jrb00013wvu@gmail.com>
2026-06-28 09:51:37 -0400

    feat(neural): add omnifold_neural package inits

---
+++ omnifold_neural/__init__.py
@@ -0,0 +1,3 @@
+"""Optional neural acceleration for district graphs and CMB scar scoring."""
+
+__all__: list[str] = []
---
+++ omnifold_neural/pinn/__init__.py
@@ -0,0 +1,5 @@
+"""Physics-informed neural metric solver."""
+
+from omnifold_neural.pinn.metric_solver import MetricPINN
+
+__all__ = ["MetricPINN"]
